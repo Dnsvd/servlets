3639c46a2659c2e17f40403bdb62bcf29d3e8aa5
Denis Denisov <dnsvd23@gmail.com>
2025-04-20 17:25:45 +0300

    Initial commit

---
+++ src/main/java/HomeWorks/Service/PostService.java
@@ -0,0 +1,33 @@
+package HomeWorks.Service;
+
+
+
+import HomeWorks.Model.Post;
+import HomeWorks.PostRepository.PostRepository;
+import HomeWorks.exception.NotFoundException;
+
+import java.util.List;
+
+public class PostService {
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Post> all() {
+        return repository.all();
+    }
+
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Post save(Post post) {
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
+}
